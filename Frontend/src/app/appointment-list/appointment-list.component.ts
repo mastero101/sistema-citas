eb1322fe02af9d19ccb4158981f2a6b654a941dd
mastero101 <mastershift_101@hotmail.com>
2024-08-21 01:35:00 -0600

    Ajuste de citas a modelo por tarjetas
    Creacion de elemento de busqueda por nombre o Fecha

--- Frontend/src/app/appointment-list/appointment-list.component.ts
+++ Frontend/src/app/appointment-list/appointment-list.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatNativeDateModule } from '@angular/material/core';
 
 import { AppointmentService } from '../services/appointment.service';
-
 
 @Component({
   selector: 'app-appointment-list',
@@ -11,12 +15,20 @@ import { AppointmentService } from '../services/appointment.service';
   imports: [
     ReactiveFormsModule,
     CommonModule,
+    FormsModule,
+    MatCardModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatNativeDateModule
   ],
   templateUrl: './appointment-list.component.html',
-  styleUrl: './appointment-list.component.scss'
+  styleUrls: ['./appointment-list.component.scss']
 })
-export class AppointmentListComponent implements OnInit{
+export class AppointmentListComponent implements OnInit {
   appointments: any[] = [];
+  searchTerm: string = '';
+  selectedDate: Date | null = null;
 
   constructor(private appointmentService: AppointmentService) { }
 
@@ -26,5 +38,16 @@ export class AppointmentListComponent implements OnInit{
     } catch (error) {
       console.error('Error fetching appointments:', error);
     }
+  }
+
+  filteredAppointments() {
+    return this.appointments.filter(appointment => {
+      const matchesName = appointment.name.toLowerCase().includes(this.searchTerm.toLowerCase());
+      const matchesDate = this.selectedDate
+        ? new Date(appointment.date).toDateString() === this.selectedDate?.toDateString()
+        : true;
+
+      return matchesName && matchesDate;
+    });
   }
 }
